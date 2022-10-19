855920168a7f981dcf008fcd616523787cee895a
geronimo-89 <konstancja.glinka@gmail.com>
2022-10-19 18:05:17 +0300

    Убраны лишние инициализации переменных

--- src/main/java/pojo/Ingredients.java
+++ src/main/java/pojo/Ingredients.java
@@ -10,7 +10,7 @@ public class Ingredients {
     @Getter @Setter
     private Boolean success;
     @Getter @Setter
-    private List<IngredientData> data = null;
+    private List<IngredientData> data;
 
     public Ingredients() {
     }
--- src/main/java/pojo/Order.java
+++ src/main/java/pojo/Order.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public class Order {
 
-    public List<String> ingredients = null;
+    public List<String> ingredients;
 
     public Order() {
     }
